dfee9d1bb6cc8cb9a86dd5dd34b4909c35573162
masudparvez00019 <masudparvez00019@gmail.com>
2026-05-04 16:26:18 +0600

    fix: vercel serverless crash fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-import { app } from "./app.js";
-import "./cron/eligibilityCron.js";
 dotenv.config({
     path: "./.env",
 });
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 connectDB()
     .then(() => {
